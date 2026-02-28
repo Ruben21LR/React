288349d056272735a2f145161ae0f869cc65d0cb
Ruben <ruben21lr@gmail.com>
2026-02-27 21:35:59 -0600

    Renderizado condicional

--- MiPrimerReact/react-app/src/App.tsx
+++ MiPrimerReact/react-app/src/App.tsx
@@ -2,11 +2,20 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const list = ["Goku", "Tanjiro", "Naruto"];
+  const list: string[] = ["Goku", "Tanjiro", "Naruto"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+
+  const contenido = list.length !== 0 && (
+    <List data={list} onSelect={handleSelect} />
+  );
+
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} />
+      {contenido}
     </Card>
   );
 }
--- MiPrimerReact/react-app/src/components/List.tsx
+++ MiPrimerReact/react-app/src/components/List.tsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 
 type Props = {
   data: string[];
+  onSelect?: (elemento: string) => void;
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
   const [index, setIndex] = useState(0);
-  const handleClick = (indice: number) => {
+  const handleClick = (indice: number, elemento: string) => {
     setIndex(indice);
+    onSelect?.(elemento);
   };
   return (
     <ul className="list-group">
       {data.map((elemento, indice) => (
         <li
-          onClick={() => handleClick(indice)}
+          onClick={() => handleClick(indice, elemento)}
           key={elemento}
           className={`list-group-item ${index == indice ? "active" : ""}`}
         >
