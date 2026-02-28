bae4fde49fa083e4b0a86c6fd13cfad87e635235
Ruben <ruben21lr@gmail.com>
2026-02-27 21:54:00 -0600

    Renderizado condicional 2

--- MiPrimerReact/react-app/src/App.tsx
+++ MiPrimerReact/react-app/src/App.tsx
@@ -2,20 +2,21 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const list: string[] = ["Goku", "Tanjiro", "Naruto"];
+  const list: string[] = ["Goku", "Naruto", "Spiderman"];
 
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo", elemento);
   };
 
-  const contenido = list.length !== 0 && (
-    <List data={list} onSelect={handleSelect} />
-  );
-
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      {contenido}
+      {/* Si la lista contiene algo (diferente de 0) imprime la lista si no el texto no hay contenido */}
+      {list.length !== 0 ? (
+        <List data={list} onSelect={handleSelect} />
+      ) : (
+        "No hay contenido"
+      )}
     </Card>
   );
 }
